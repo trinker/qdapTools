1c92c0f5fb40516b008d931afaf1f85bf17fc71d
Tyler Rinker <tyler.rinker@kangarootime.com>
2023-05-09 23:37:25 -0400

    cran release

--- inst/extra_statdoc/readme.R
+++
@@ -1,4 +0,0 @@
-<p><img src="https://dl.dropbox.com/u/61803503/qdapicon.png" alt="qdapicon"/><br/>
-<a href="https://github.com/trinker/qdapTools">qdapTools</a> is an R package that contains tools associated with the qdap package that may be useful outside of the context of text analysis.</p>
-
-<p>Download the development version of qdapTools <a href="https://github.com/trinker/qdapTools/">here</a> 
